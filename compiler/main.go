7e2f4a2b084f169cef37be9039be72517ef70767
Mateus Nakajo <mateusnakajo@gmail.com>
2018-09-30 16:10:36 -0300

    get args from command line

--- compiler/main.go
+++ compiler/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+	args := os.Args[1:]
+	switch {
+	case len(args) > 1:
+		fmt.Println("Usage: basic [script]")
+	case len(args) == 1:
+		fmt.Println("ok")
+	case len(args) == 0:
+		fmt.Println(">>")
+	}
 }
