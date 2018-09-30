5b1c4a8a3e9f8aee6386b92ba6590e59458ea4a7
Mateus Nakajo <mateusnakajo@gmail.com>
2018-09-30 16:30:38 -0300

    reading file line by line

--- compiler/main.go
+++ compiler/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
+	"log"
 	"os"
 )
 
@@ -12,12 +13,25 @@ func main() {
 	case len(args) > 1:
 		fmt.Println("Usage: basic [script]")
 	case len(args) == 1:
-		dat, err := ioutil.ReadFile(args[0])
-		if err != nil {
-			panic(err)
-		}
-		fmt.Print(string(dat))
+		compileFile(args[0])
 	case len(args) == 0:
 		fmt.Println(">>")
+	}
+}
+
+func compileFile(fileName string) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
 	}
 }
