34b10bc811edd19ae836bc9a2ef57a2149f85e6c
Mateus Nakajo <mateusnakajo@gmail.com>
2018-09-30 20:41:32 -0300

    start scanToken logic

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -2,20 +2,37 @@ package lexer
 
 import (
 	"fmt"
-	"strings"
 )
 
 type Lexer struct {
-	Source string
-	Tokens []Token
+	Source  string
+	Tokens  []Token
+	Start   int
+	Current int
 }
 
 func (l Lexer) ScanTokens() []Token {
-	lines := strings.Split(l.Source, "\n")
-	for _, line := range lines {
-		for _, char := range line {
-			fmt.Print(string(char))
-		}
+	for !l.isAtEnd() {
+		l.Start = l.Current
+		l.scanToken()
 	}
+
 	return nil
+}
+
+func (l Lexer) isAtEnd() bool {
+	return l.Current >= len(l.Source)
+}
+
+func (l *Lexer) scanToken() {
+	c := l.advance()
+	switch c {
+	case "(":
+		fmt.Print("(\n")
+	}
+}
+
+func (l *Lexer) advance() string {
+	l.Current++
+	return string(l.Source[l.Current-1])
 }
